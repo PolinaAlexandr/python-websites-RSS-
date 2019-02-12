c503177b13ba4e462df74f682a5eaa94338821eb
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-13 01:06:08 +0300

    Added classes for parsing RSS  xml elements

---
+++ article.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+class Article:
+    def __init__(self, title, pub_date):
+        self.title = title
+        self.pub_date = pub_date
+
+    def __str__(self):
+        return 'Title: {}\tDate: {}'.format(self.title, self.pub_date)
+    
+class TutArticle(Article):
+    def __init__(self, tree_item):        
+        title = tree_item.find('title').text
+        # Mon, 11 Feb 2019 08:40:00 +0300
+        pub_date = datetime.strptime(
+            tree_item.find('pubDate').text,
+            '%a, %d %b %Y %H:%M:%S %z'
+        )
+
+        super().__init__(title, pub_date)
+        self.category = tree_item.find('category').text
+
+    def __str__(self):
+        return '{}\tCategory: {}'.format(super().__str__(), self.category)
+
+# class RedditArticle(Article):
+#     def __init__(self, tree_item):
+#          super().__init__(self, title, pub_date, tree_item)
+#          self.tree_item = tree_item
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
 
+from article import TutArticle
+
 r = requests.get('https://news.tut.by/rss/it.rss')
 
 tree = ET.fromstring(r.text)
 
-news = [el.find('title').text for el in tree.iter(tag='item')]
+news = [TutArticle(el) for el in tree.iter(tag='item')]
 
 for item in news[:5]:
     print(item)
