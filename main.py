8a76b5741786a3591132be5a0496f0b0a956062c
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-13 14:32:17 +0300

    Added wibsites variaty from url(input)(untested)

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import requests
+import urllib.request
 import xml.etree.ElementTree as ET
 
+import article
 from article import TutArticle
+from article import RedditArticle
 
-r = requests.get('https://news.tut.by/rss/it.rss')
+m = urllib(str(input))
+r = urllib.requests.get(m)
+if r.find("tut") is True:
+    tree = ET.fromstring(r.text)
+    news = [TutArticle(el) for el in tree.iter(tag='item')]
 
-tree = ET.fromstring(r.text)
+elif r.find("reddit") is True:
+    tree = ET.fromstring(r.text)
+    news = [RedditArticle(el) for el in tree.iter(tag='entry')]
 
-news = [TutArticle(el) for el in tree.iter(tag='item')]
+else:
+    # tree = ET.fromstring(r.text)
+    # news = [article(el) for el in tree.iter(tag=str(input))]
+    pass
+
 
 for item in news[:5]:
     print(item)
